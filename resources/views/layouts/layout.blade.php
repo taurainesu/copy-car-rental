42cdbdb2e6374f03838d1991a673e219d1cb4efa
Paul Kofa <paulkofa@gmail.com>
2020-03-02 14:04:54 +0200

    test

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -105,16 +105,12 @@ body,a,h1,h2,h3,h4,.header,p,button,input,.search{
     <div class="ui container">
       <div class="ui stackable inverted divided equal height stackable grid">
         <div class="three wide column">
-          <h4 class="ui inverted header">About</h4>
-          <div class="ui inverted link list">
-            <a href="#" class="item">Sitemap</a>
-            <a href="#" class="item">Contact Us</a>
-            <a href="#" class="item">Religious Ceremonies</a>
-            <a href="#" class="item">Gazebo Plans</a>
-          </div>
+          <h4 class="ui inverted header">Address</h4>
+          
+          <p>Harare CBD</p>
         </div>
         <div class="three wide column">
-          <h4 class="ui inverted header">Services</h4>
+          <h4 class="ui inverted header">Get in touch</h4>
           <div class="ui inverted link list">
             <a href="#" class="item">Banana Pre-Order</a>
             <a href="#" class="item">DNA FAQ</a>
