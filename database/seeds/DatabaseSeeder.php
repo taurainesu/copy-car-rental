3a59be33acb87c78d3d26c5d16b4214c063d03b1
Daveson Mukuna <mkunadavy@gmail.com>
2020-04-08 12:55:02 +0200

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,12 +16,47 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         DB::table('users')->insert([
-            'name' => Str::random(10),
-            'address' => Str::random(10),
-            'licenseNo' => Str::random(10),
-            'sex' => Str::random(10),
-            'email' => Str::random(10).'@gmail.com',
-            'password' => Hash::make('password'),
+            'id'=> "1",
+            'name'=> "Administrator",
+            'email'=> "admin@admin.com",
+            'phone'=> "0771111111",
+            'sex'=> "Male",
+            'age'=> "27",
+            'password'=>Hash::make('taurayinesu'),
+            'address'=> "03 Chancellor Rd Harare",
+            'nationality'=> "Zimbabwean",
+            'country'=> "Zimbabwe",
+            'licenseNo'=> "HFJ8392",
+            'is_admin'=> 1,
+        ]);
+
+        DB::table('cars')->insert([
+        "id"=> "1",
+         "brand"=> "Toyota",
+         "model"=> "Harrier",
+         "location"=> "Harare",
+         "vehicle_registration"=> "ADC3899",        
+         "physical_address"=> "53 mukuna road",     
+         "description"=> "cool car",
+         "year"=> "2011",
+         "cordinates"=> null,
+         "milage"=> "10000",
+         "type"=> "sedan",
+         "daily_rate"=>300.00,
+         "user_id"=> "1",
+         "imageUrl"=> "/images/cars/1584347030.png",
+         "imageUrl1"=> "/images/cars/15843470301.png",
+         "imageUrl2"=> "/images/cars/15843470302.png",
+         "imageUrl3"=> "/images/cars/15843470303.png",
+         "imageUrl4"=> "/images/cars/15843470314.png",
+         "fuel_type"=> "disiel",
+         "engine_capacity"=> "2",
+         "color"=> "Green",
+         "seats"=> "6",
+         "transmission"=> "manual",
+         "done"=> null,
+         "status"=> "pending",
+         "deleted_at"=> null,
         ]);
     }
 }
